2aac5aaab902df0d3c2499ffb462311bb5a6a0c7
Yaroslav Rozhilo <yaro@ulis.nld.onet.net.ua>
2013-07-13 01:34:14 +0300

    Buffer initialized in web.js that reads from index.html

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html');
+var buf = new Buffer (20);
+buf = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send(buf.toString());
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
