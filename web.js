d6706c0e59de56ce58e224fda896662dc30d5540
Yaroslav Rozhilo <yaro@ulis.nld.onet.net.ua>
2013-07-13 00:59:08 +0300

    Error ) in web.js that reads from index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var buf = fs.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send(buf.toString();
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
